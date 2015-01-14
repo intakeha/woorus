9a53ee79f9e551f46909155c937430e8594f2999
Chonabot Intakeha <chon@woorus.com>
2015-01-14 10:16:40 -0800

    Added custom.css file

--- app/views/hello.php
+++ app/views/hello.php
@@ -25,7 +25,7 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen">
     
     <!-- Plugins CSS Styles -->
-    <link rel="stylesheet" href="css/plugins.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/plugin.css" type="text/css" media="screen">
   
     <!-- Woorus CSS Styles -->
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
