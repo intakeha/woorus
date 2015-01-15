6cd47828d27ed2f0251c5cc6a20f51898d5e2be8
Chonabot Intakeha <chon@woorus.com>
2015-01-15 12:24:13 -0800

    Change CSS reference

--- app/views/hello.php
+++ app/views/hello.php
@@ -22,7 +22,7 @@
 
     
     <!-- Bootstrap CSS Styles -->
-	<link media="screen" type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     
     <!-- Plugins CSS Styles -->
     <link rel="stylesheet" href="css/plugin.css" type="text/css" media="screen">
